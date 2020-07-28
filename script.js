0bdcbefd8b5e32107b0e528e1a76bef993b3a58f
ALMaclaine <almaclaine@gmail.com>
2020-07-27 17:29:36 -0700

    add code to update sliderValues on input events and update display background-color

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const colorViewer = document.getElementById('color-viewer');
 
 const sliderValues = {
-    r: 0,
-    g: 0,
-    b: 0
+    r: 255,
+    g: 255,
+    b: 255
 };
 
 const sliders = Array.from(document.getElementsByClassName('color-slider'));
@@ -19,7 +19,10 @@ sliders.map(e => {
     } else {
         throw 'Invalid id, should be red, green or blue';
     }
-    setColorViewerBackgroundFromSliderValues();
+    e.addEventListener('input', (e) => {
+        updateSliderValuesFromInputEvent(e);
+        setColorViewerBackgroundFromSliderValues();
+    });
 });
 
 function setElementBackgroundColor(elem, r, g, b) {
@@ -32,4 +35,18 @@ function setColorViewerBackground(r, g, b) {
 
 function setColorViewerBackgroundFromSliderValues() {
     setColorViewerBackground(sliderValues.r, sliderValues.g, sliderValues.b);
+}
+
+function updateSliderValuesFromInputEvent(e) {
+    const target = e.currentTarget;
+    const id = target.id;
+    if(id === 'red') {
+        sliderValues.r = target.value;
+    } else if(id === 'green') {
+        sliderValues.g = target.value;
+    } else if(id === 'blue') {
+        sliderValues.b = target.value;
+    } else {
+        throw 'Invalid id, should be red, green or blue';
+    }
 }
