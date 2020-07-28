ac6485863497aa63fe942b2c51911558eebb287f
ALMaclaine <almaclaine@gmail.com>
2020-07-27 17:22:37 -0700

    add helper functions to change colorViewer background color

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const colorViewer = document.getElementById('color-viewer');
+
+function setElementBackgroundColor(elem, r, g, b) {
+    elem.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function setColorViewerBackground(r, g, b) {
+    setElementBackgroundColor(colorViewer, r, g, b);
+}
+
+setColorViewerBackground(100, 100, 100);
