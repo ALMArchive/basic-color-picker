1b2c587b46f2a41be0965ed058f3eddb1d335473
ALMaclaine <almaclaine@gmail.com>
2020-07-27 17:26:41 -0700

    add code to set html slider values to sliderValues initial values

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const colorViewer = document.getElementById('color-viewer');
 
+const sliderValues = {
+    r: 0,
+    g: 0,
+    b: 0
+};
+
+const sliders = Array.from(document.getElementsByClassName('color-slider'));
+sliders.map(e => {
+    const id = e.id;
+    if(id === 'red') {
+        e.value = sliderValues.r;
+    } else if(id === 'green') {
+        e.value = sliderValues.g;
+        sliderValues.g = e.value;
+    } else if(id === 'blue') {
+        e.value = sliderValues.b;
+    } else {
+        throw 'Invalid id, should be red, green or blue';
+    }
+    setColorViewerBackgroundFromSliderValues();
+});
+
 function setElementBackgroundColor(elem, r, g, b) {
     elem.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
@@ -8,4 +30,6 @@ function setColorViewerBackground(r, g, b) {
     setElementBackgroundColor(colorViewer, r, g, b);
 }
 
-setColorViewerBackground(100, 100, 100);
+function setColorViewerBackgroundFromSliderValues() {
+    setColorViewerBackground(sliderValues.r, sliderValues.g, sliderValues.b);
+}
